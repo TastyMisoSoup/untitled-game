57c8c8b66d04a60b8665ccf58d2356d4fd4c5d5f
TastyMisoSoup <tastymisosoup0@gmail.com>
2025-07-03 00:40:24 +0200

    added pistol weapon, added animation to it, started experimenting with line2d and raycasts, added tilemap (better sprites required for both map and pistol)

--- code/pistol.gd
+++ code/pistol.gd
@@ -1,4 +1,16 @@
 extends "./weapon.gd"
 
 func action():
-	print("no")
+	var position: Vector2;
+	$AnimationPlayer.play("shoot")
+	if(get_local_mouse_position().y>-60):
+		position = Vector2(get_local_mouse_position().x,-60);
+	elif(get_local_mouse_position().y<-200):
+		position = Vector2(get_local_mouse_position().x,-200);
+	else:
+		position = get_local_mouse_position();
+	$Line2D.set_point_position(1,position)
+	$RayCast2D.set_target_position(position)
+	$RayCast2D.set_enabled(true)
+	print($RayCast2D.is_colliding())
+	$RayCast2D.set_enabled(false)
