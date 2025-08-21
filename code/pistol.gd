610806a746ceb3760db3ba1223f7a8651d4a4602
TastyMisoSoup <tastymisosoup0@gmail.com>
2025-08-21 18:25:52 +0200

    working on collision of bullets

--- code/pistol.gd
+++ code/pistol.gd
@@ -9,10 +9,11 @@ func action():
 		#position = Vector2(get_local_mouse_position().x,-200);
 	else:
 		position = get_local_mouse_position();
-	position.x = position.x + (randi_range(-40,40))
-	position.y = position.y + (randi_range(-10,10))
-	$Line2D.set_point_position(1,position)
+	#position.x = position.x + (randi_range(-40,40))
+	#position.y = position.y + (randi_range(-10,10))
 	$RayCast2D.set_target_position(position)
-	$RayCast2D.set_enabled(true)
-	print($RayCast2D.is_colliding())
-	$RayCast2D.set_enabled(false)
+	if $RayCast2D.is_colliding():
+		print($RayCast2D.get_collision_point())
+		$Line2D.set_point_position(1,($RayCast2D.get_collision_point()-$Line2D.global_position))
+	else:
+		$Line2D.set_point_position(1,position)
