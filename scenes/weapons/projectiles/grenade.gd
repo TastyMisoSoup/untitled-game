b8064853f62f901981e6ae3fd7ce83361b5f5605
VarholakP <tastymisosoup0@gmail.com>
2025-12-03 19:37:13 +0100

    fix rotation of grenade

--- scenes/weapons/projectiles/grenade.gd
+++ scenes/weapons/projectiles/grenade.gd
@@ -3,7 +3,6 @@ class_name Grenade
 
 func _ready() -> void:
 	position = start_position
-	look_at(global_position + direction)
 	add_to_group(team)
 	velocity = direction * speed
 	$Timer.start(timer)
